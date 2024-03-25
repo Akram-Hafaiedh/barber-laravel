bd81d81b6d3d143d10aac93bec1aa8761d72a435
Akram <hfaiedhakram@gmail.com>
2024-03-25 08:30:00 +0100

    added test accounts in the login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,8 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block w-full mt-1" type="email" name="email" :value="old('email')" required
+                autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -16,10 +17,8 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input id="password" class="block w-full mt-1" type="password" name="password" required
+                autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -27,14 +26,17 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox"
+                    class="text-indigo-600 border-gray-300 rounded shadow-sm dark:bg-gray-900 dark:border-gray-700 focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                    name="remember">
+                <span class="text-sm text-gray-600 ms-2 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
@@ -44,4 +46,35 @@
             </x-primary-button>
         </div>
     </form>
+
+    <div class="mt-6 text-sm text-center text-gray-600 dark:text-gray-400">
+        Or login with one of these pre-made accounts
+    </div>
+    <div class="p-4 mt-2 bg-gray-100 rounded-md dark:bg-gray-700">
+        <div class="flex items-center justify-center mb-2">
+            <span class="text-sm font-medium text-gray-600 dark:text-white">User</span>
+        </div>
+        <div class="flex items-center justify-center mb-2">
+            <span class="text-sm font-medium text-gray-600 dark:text-gray-200">Email</span>
+            <span class="ml-2 text-sm font-medium text-indigo-600 dark:text-indigo-300">test@example.com</span>
+        </div>
+        <div class="flex items-center justify-center">
+            <span class="text-sm font-medium text-gray-600 dark:text-gray-200">Password</span>
+            <span class="ml-2 text-sm font-medium text-indigo-600 dark:text-indigo-300">password</span>
+        </div>
+    </div>
+    <div class="p-4 mt-2 bg-gray-100 rounded-md dark:bg-gray-700">
+
+        <div class="flex items-center justify-center mb-2">
+            <span class="text-sm font-medium text-gray-600 dark:text-white">Admin</span>
+        </div>
+        <div class="flex items-center justify-center mb-2">
+            <span class="text-sm font-medium text-gray-600 dark:text-gray-200">Email</span>
+            <span class="ml-2 text-sm font-medium text-indigo-600 dark:text-indigo-300">admin@example.com</span>
+        </div>
+        <div class="flex items-center justify-center">
+            <span class="text-sm font-medium text-gray-600 dark:text-gray-200">Password</span>
+            <span class="ml-2 text-sm font-medium text-indigo-600 dark:text-indigo-300">password</span>
+        </div>
+    </div>
 </x-guest-layout>
